0ef7aabb1ba8806fec7f219fc650d1e5adb7fccb
Giang Phan <gphan485@gmail.com>
2020-07-31 10:25:34 +0700

    add user profile

--- app/src/main/java/phanbagiang/com/chatapp/activities/ProfileActivity.java
+++ app/src/main/java/phanbagiang/com/chatapp/activities/ProfileActivity.java
@@ -1,16 +1,34 @@
 package phanbagiang.com.chatapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import phanbagiang.com.chatapp.R;
+import phanbagiang.com.chatapp.model.User;
 
 public class ProfileActivity extends AppCompatActivity {
     Toolbar toolbar;
+    CircleImageView image;
+    TextView txtname;
 
+    //Firebase
+    FirebaseUser mUser;
+    DatabaseReference mReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,31 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        addControls();
+    }
+    private void addControls(){
+        image=findViewById(R.id.profile_image);
+        txtname=findViewById(R.id.profile_name);
+        mUser= FirebaseAuth.getInstance().getCurrentUser();
+        mReference= FirebaseDatabase.getInstance().getReference("users");
+        mReference.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user=dataSnapshot.getValue(User.class);
+                if(user.getImage().equals("default")){
+                    image.setImageResource(R.mipmap.ic_launcher);
+                }
+                else{
+                    Glide.with(getApplicationContext()).load(user.getImage()).into(image);
+                }
+                txtname.setText(user.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
